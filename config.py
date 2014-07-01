c853bc29f487f40165c38236a9b58e6d70e12190
Bill Mei <billmei@users.noreply.github.com>
2014-07-01 13:26:45 -0400

    new files

--- config.py
+++ config.py
@@ -1,2 +1,10 @@
 CSRF_ENABLED = True
 SECRET_KEY = '79s3+vi9m!$c@d+rk7@_zj0n+a5#q42@a-j$08f!-)s26cyvtx'
+
+OPENID_PROVIDERS = [
+	{ 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
+	{ 'name': 'Yahoo', 'url': 'https://me.yahoo.com' },
+	{ 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
+	{ 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
+	{ 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
+]
